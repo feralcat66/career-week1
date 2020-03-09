368ba8fd1d19a07d3a1c0c8752fe609e1d013885
Rex Bergeron <rexbergeron@comcast.net>
2020-03-09 16:43:43 -0700

    all done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber, isArray, isBoolean, isFunction, isObject, isString } = require('./lib/types.js');
+
+console.log(isNumber(4));
+console.log(isArray([]));
+console.log(isBoolean(true));
+console.log(isFunction(4));
+console.log(isObject({}));
+console.log(isString('hi'));
