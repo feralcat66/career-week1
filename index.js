4825d0e5f9c621bcdc153776d331b5e96251c988
Rex Bergeron <rexbergeron@comcast.net>
2020-03-10 16:48:08 -0700

    should have committed a while ago

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const { isNumber, isArray, isBoolean, isFunction, isObject, isString } = require('./lib/types.js');
+// const { isNumber, isArray, isBoolean, isFunction, isObject, isString } = require('./lib/types.js');
 
-console.log(isNumber(4));
-console.log(isArray([]));
-console.log(isBoolean(true));
-console.log(isFunction(4));
-console.log(isObject({}));
-console.log(isString('hi'));
+// console.log(isNumber(4));
+// console.log(isArray([]));
+// console.log(isBoolean(true));
+// console.log(isFunction(4));
+// console.log(isObject({}));
+// console.log(isString('hi'));
+
+const { nameValidator, ageValidator, heightValidator, eyeColorValidator  } = require('./lib/Validator.js');
+
+const jim = {
+  name: 'jim',
+  age: '69',
+  eyeColor: 'green'
+};
+
+console.log(nameValidator.validate(jim));
+console.log(ageValidator.validate(jim));
+console.log(heightValidator.validate(jim));
+console.log(eyeColorValidator.validate(jim));
