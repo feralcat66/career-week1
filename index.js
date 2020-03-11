74ff63b484a1a1ec08f5ddc82926b989a29cb8e3
Rex Bergeron <rexbergeron@comcast.net>
2020-03-11 10:10:56 -0700

    travis better pass

--- index.js
+++ index.js
@@ -7,15 +7,15 @@
 // console.log(isObject({}));
 // console.log(isString('hi'));
 
-const { nameValidator, ageValidator, heightValidator, eyeColorValidator  } = require('./lib/Validator.js');
+// const { nameValidator, ageValidator, heightValidator, eyeColorValidator  } = require('./lib/Validator.js');
 
-const jim = {
-  name: 'jim',
-  age: '69',
-  eyeColor: 'green'
-};
+// const jim = {
+//   name: 'jim',
+//   age: '69',
+//   eyeColor: 'green'
+// };
 
-console.log(nameValidator.validate(jim));
-console.log(ageValidator.validate(jim));
-console.log(heightValidator.validate(jim));
-console.log(eyeColorValidator.validate(jim));
+// console.log(nameValidator.validate(jim));
+// console.log(ageValidator.validate(jim));
+// console.log(heightValidator.validate(jim));
+// console.log(eyeColorValidator.validate(jim));
