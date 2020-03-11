f9b42556556cc7b0d04ee937812cbbe23fe3fbe5
Rex Bergeron <rexbergeron@comcast.net>
2020-03-11 11:09:21 -0700

    fixed things for travis

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { isNumber, isArray, isBoolean, isFunction, isObject, isString } = require('./lib/types.js');
+// const { isNumber, isArray, isBoolean, isFunction, isObject, isString } = require('./lib/types.js');
 
-console.log(isNumber(4));
-console.log(isArray([]));
-console.log(isBoolean(true));
-console.log(isFunction(4));
-console.log(isObject({}));
-console.log(isString('hi'));
+// console.log(isNumber(4));
+// console.log(isArray([]));
+// console.log(isBoolean(true));
+// console.log(isFunction(4));
+// console.log(isObject({}));
+// console.log(isString('hi'));
